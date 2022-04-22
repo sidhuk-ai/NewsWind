3b845d067c74ca172a2690a769b699b58128f172
Siddharth184 <77151100+Siddharth184@users.noreply.github.com>
2022-04-22 12:42:53 +0530

    added copy button

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import Favorite from '@mui/icons-material/Favorite';
-import SpeedDials from './SpeedDial';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import ShareIcon from '@mui/icons-material/Share';
+import { Snackbar } from '@mui/material';
+import { Alert } from '@mui/material';
 
 function NewsItem(props) {
     let { title, description, imageurl, newsUrl, author, date, mode} = props;
+    const [open, setOpen] = useState(false);
+    const isCopied = () => {
+        setOpen(true)
+    }
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }else{
+          setOpen(false);
+        }
+    };
     return (
         <>
+            <Snackbar open={open} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{horizontal:'center',vertical:'bottom'}}>
+                <Alert severity="success" sx={{ width: '100%' }}>
+                    Link of News Copied to Clipboard
+                </Alert>
+            </Snackbar>
             <div className="card container" style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",backgroundColor: mode==="dark"?"#2a2b2d":"white" }}>
                 <img src={imageurl ? imageurl : 'https://static.vecteezy.com/system/resources/previews/000/228/657/original/top-headlines-news-themem-background-vector.jpg'} className="card-img-top mt-3" alt="img" width="305" height="172" />
                 <div className="card-body">
@@ -19,10 +38,11 @@ function NewsItem(props) {
                         <a href={newsUrl} id='url' target="_blank" rel="noreferrer" className="btn btn-sm btn-primary container mt-[15px]">Read More<svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1 mb-1 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                         </svg></a>
-                        <Checkbox className='!mt-[15px] ml-1' color='error' icon={<FavoriteBorder sx={{fontSize: 25}} color={`${mode==='dark'?'default':'black'}`}/>} checkedIcon={<Favorite sx={{fontSize: 25}} color={`${mode==='dark'?'default':'black'}`}/>} />
+                        <Checkbox className='ml-1 !mt-[15px]' color='error' icon={<FavoriteBorder sx={{fontSize: 25, color:`${mode==='dark'?'white':'black'}`}}/>} checkedIcon={<Favorite sx={{fontSize: 25}} />} />
+                        <CopyToClipboard text={newsUrl}>
+                            <ShareIcon className='ml-1 !mt-[15px]' sx={{fontSize: 25, color:`${mode==='dark'?'white':'black'}`}} onClick={isCopied}/>
+                        </CopyToClipboard>
                     </div>
-                    <hr className='m-3'/>
-                    <SpeedDials/>
                 </div>
             </div>
         </>
