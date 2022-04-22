dd85395247a2d154115f332d43218fd4eb1fc589
Siddharth184 <77151100+Siddharth184@users.noreply.github.com>
2022-04-22 13:03:43 +0530

    added gh pages

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -29,8 +29,7 @@ const Loading = (props) => {
                 
                 <div className="col-md-4 mb-4">
                     <div className="card container" aria-hidden="true" style={{backgroundColor: mode==="light"?"white":"rgb(97 92 92)"}}>
-                        {/* <svg className="bd-placeholder-img card-img-top m-[10px]" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg> */}
-                        <Skeleton
+\                        <Skeleton
                          variant="rectangular" 
                          className='mt-1' 
                          width={347.1} 
@@ -39,15 +38,9 @@ const Loading = (props) => {
                         />
                         <div className="card-body text-center">
                             <h5 className="card-title placeholder-glow">
-                                {/* <span className="placeholder col-6"></span> */}
                                 <Skeleton animation="wave" />
                             </h5>
                             <p className="card-text placeholder-glow">
-                                {/* <span className="placeholder col-7"></span>
-                                <span className="placeholder col-4"></span>
-                                <span className="placeholder col-4"></span>
-                                <span className="placeholder col-6"></span>
-                                <span className="placeholder col-8"></span> */}
                                 <Skeleton animation="wave" />
                                 <Skeleton animation="wave" />
                                 <Skeleton animation="wave" />
@@ -62,19 +55,12 @@ const Loading = (props) => {
                 </div>
                 <div className="col-md-4 mb-4">
                     <div className="card container" aria-hidden="true" style={{backgroundColor: mode==="light"?"white":"rgb(97 92 92)"}}>
-                        {/* <svg className="bd-placeholder-img card-img-top m-[10px]" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg> */}
                         <Skeleton variant="rectangular" className='mt-1' width={347.1} height={180} animation='wave'/>
                         <div className="card-body text-center">
                             <h5 className="card-title placeholder-glow">
-                                {/* <span className="placeholder col-6"></span> */}
                                 <Skeleton animation="wave" />
                             </h5>
                             <p className="card-text placeholder-glow">
-                                {/* <span className="placeholder col-7"></span>
-                                <span className="placeholder col-4"></span>
-                                <span className="placeholder col-4"></span>
-                                <span className="placeholder col-6"></span>
-                                <span className="placeholder col-8"></span> */}
                                 <Skeleton animation="wave" />
                                 <Skeleton animation="wave" />
                                 <Skeleton animation="wave" />
